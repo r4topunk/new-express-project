1bc5e5ed799b0e7f6ee8cce7ab82a213804d0580
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-11-17 22:31:34 -0300

    feat: update Bearer cookie domain and redirect URL

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -37,8 +37,8 @@ router.get("/jwt/:jwt", async (req, res) => {
       });
     }
 
-    res.cookie('Bearer', req.params.jwt, { httpOnly: true });
-    return res.redirect(`https://b9b5-186-209-202-32.ngrok-free.app`);
+    res.cookie('Bearer', req.params.jwt, { httpOnly: true, domain: 'b9b5-186-209-202-32.ngrok-free.app' });
+    return res.redirect(result[0].url);
   } catch (error) {
     console.error(error);
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
