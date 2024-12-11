352154a7113320deb0c29be78ae801457ce61061
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-11 20:05:43 -0300

    chore: add columns to database

--- src/drizzle/schema/redirects.ts
+++ src/drizzle/schema/redirects.ts
@@ -1,13 +1,18 @@
-import { pgTable, text, uuid, timestamp, integer } from 'drizzle-orm/pg-core';
+import { pgTable, text, uuid, timestamp, integer } from "drizzle-orm/pg-core";
 
-export const redirects = pgTable('redirects', {
-  uuid: uuid('uuid').primaryKey().defaultRandom(),
-  url: text('url').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
-  description: text('description'),
-  number: integer('number'),
-  group: integer('group'),
-  xLocation: integer('x_location'),
-  zLocation: integer('z_location')
+export const redirects = pgTable("redirects", {
+  uuid: uuid("uuid").primaryKey().defaultRandom(),
+  url: text("url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  description: text("description"),
+  number: integer("number"),
+  group: integer("group"),
+  xLocation: integer("x_location"),
+  zLocation: integer("z_location"),
+  phyditalContract: text("phydital_contract"),
+  phyditalTokenId: integer("phydital_token_id"),
+  poapContract: text("poap_contract"),
+  poapTokenId: integer("poap_token_id"),
+  chainId: integer("chain_id"),
 });
