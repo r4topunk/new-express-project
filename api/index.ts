6e78d32f666e203ce34d60b11fe61031159e0d22
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2025-01-14 09:16:47 -0300

    feat: add CORS middleware and update preferred port number

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,35 @@
-import compression from 'compression';
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import compression from "compression";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
-import { errorHandler } from '../src/middlewares/handleError';
-import routes from '../src/routes/index';
+import { errorHandler } from "../src/middlewares/handleError";
+import routes from "../src/routes/index";
 
-const appName = 'moncy-express-ts-starter';
-const preferredPortNumber = 3000;
+const appName = "moncy-express-ts-starter";
+const preferredPortNumber = 3001;
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
-morgan.token('url', (req, res) => decodeURIComponent(req.url ?? ''));
-app.use(morgan('tiny'));
+morgan.token("url", (req, res) => decodeURIComponent(req.url ?? ""));
+app.use(morgan("tiny"));
 app.use(compression());
 app.use(helmet());
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
-app.use('/', routes);
+app.use("/", routes);
 app.use(errorHandler);
 
 app.listen(preferredPortNumber, () => {
   console.log(
-    `${appName} app started on http://localhost:${preferredPortNumber}`,
+    `${appName} app started on http://localhost:${preferredPortNumber}`
   );
 });
